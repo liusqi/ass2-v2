61923a0a32944f3901f3467dd055833a74c54631
Nav Bhatti <bhatti.nav@gmail.com>
2015-03-19 17:10:44 -0700

    added mdfs

---
+++ a2/Migrations/201503200004096_LookupInitial.cs
@@ -0,0 +1,16 @@
+namespace a2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class LookupInitial : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
